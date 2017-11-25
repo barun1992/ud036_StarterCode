855ece78fece22a5ff389d0a8058c36b2a4a1ada
barun1992 <33389360+barun1992@users.noreply.github.com>
2017-11-25 18:31:25 +0530

    Add files via upload

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,9 +1,29 @@
 import media
 import fresh_tomatoes
-toy_story=media.Movie("Toy Story 2","Toys Come to Life","https://upload.wikimedia.org/wikipedia/en/c/c0/Toy_Story_2.jpg","https://www.youtube.com/watch?v=Lu0sotERXhI")
-avatar=media.Movie("Avatar","A marine on an alien planet","https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg","https://www.youtube.com/watch?v=5PSNL1qE6VY")
-school_of_rock=media.Movie("School of Rock","Story of a rock band","https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg","https://youtu.be/5afGGGsxvEA")
-ratatouille=media.Movie("Ratatouille","Movie of a mouse","https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg","https://youtu.be/fIjHfW6y4Mg")
+"""Submit values for Toy Story Movie"""
+toy_story = media.Movie(
+    "Toy Story 2",
+    "Toys Come to Life",
+    "https://upload.wikimedia.org/wikipedia/en/c/c0/Toy_Story_2.jpg",
+    "https://www.youtube.com/watch?v=Lu0sotERXhI")
+"""Submit values for Avatar movie"""
+avatar = media.Movie(
+    "Avatar",
+    "A marine on an alien planet",
+    "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+    "https: // www.youtube.com/watch?v=5PSNL1qE6VY")
+"""Submit values for School of Rock Movie"""
+school_of_rock = media.Movie(
+    "School of Rock",
+    "Story of a rock band",
+    "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+    "https://youtu.be/5afGGGsxvEA")
+"""Submit values for Ratatouille Movie"""
+ratatouille = media.Movie(
+    "Ratatouille",
+    "Movie of a mouse",
+    "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+    "https://youtu.be/fIjHfW6y4Mg")
 
-movies = [toy_story,avatar,school_of_rock,ratatouille]
+movies = [toy_story, avatar, school_of_rock, ratatouille]
 fresh_tomatoes.open_movies_page(movies)
